632af97a006dd38ef56fd01a38da0aa39fb435c1
Mygithubayaa <ayaalmouiz11@gmail.com>
2023-09-13 17:00:07 -0700

    mycommit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two num
+ *
+ * @num1: the first num
+ *
+ * @num2: the seconde num
+ *
+ * Return: int value
+*/
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
