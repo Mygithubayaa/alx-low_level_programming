be85c3cf5c0fdd18bcb2c312d02f94c1a684f175
Mygithubayaa <ayaalmouiz11@gmail.com>
2023-09-21 00:17:33 -0700

    ay

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,23 @@
 #include "main.h"
 
 /**
+* positive_or_negative- afunction
+* i - an intger
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
+int positive_or_negative(int i)
+{
+	if (i > 0)
+	printf("%d is positive\n", i);
+
+	else if (i == 0)
+	printf("%d is zero\n", i);
+
+	else
+	printf("%d is negative\n", i);
+}
 int main(void)
 {
 	int i;
