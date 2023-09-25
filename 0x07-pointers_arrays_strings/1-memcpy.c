f6ee902a40b7be654765689c6cb08cf541d5e107
Mygithubayaa <ayaalmouiz11@gmail.com>
2023-09-25 11:50:51 -0700

    mycommit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy-copying
+ *
+ * @dest:string
+ *
+ * @src:string
+ * @n:int
+ * Return:dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *dest_ptr = dest;
+	unsigned int h;
+
+	for (h = 0; h < n; h++)
+	{
+		*dest_ptr++ = *src++;
+		n--;
+	}
+	return (dest);
+}
