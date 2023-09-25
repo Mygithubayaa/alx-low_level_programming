fc16c762630114a45ca0d7a9cf6380b6e39309f1
Mygithubayaa <ayaalmouiz11@gmail.com>
2023-09-25 16:16:28 -0700

    mycommit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strstr-print substring
+ *
+ * @haystack:string
+ * @needle:string
+ * Return:char
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+	for (j = 0; needle[j] != '\0'; j++)
+	{
+		if (haystack[i + j] != needle[j])
+		{
+			break;
+		}
+	}
+	if (needle[j] == '\0')
+	{
+		return (&haystack[i]);
+	}
+	}
+	return (NULL);
+}
