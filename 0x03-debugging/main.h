fe2e6d9e8ad0792b55e38cc689eec5381e6db86b
Mygithubayaa <ayaalmouiz11@gmail.com>
2023-09-25 17:49:38 -0700

    mycommit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,5 +7,5 @@ void positive_or_negative(int n);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
-
+int _putchar(char c);
 #endif /* MAIN_H */
