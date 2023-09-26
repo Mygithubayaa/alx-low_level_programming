f2c50c8fe18670352c0a20aaadc70f3c00d0096d
Mygithubayaa <ayaalmouiz11@gmail.com>
2023-09-25 17:03:38 -0700

    mycommit

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums- sum oftwo matrix
+ * @a:int
+ * @size:int
+ * Return:void
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int sum1 = 0;
+	int sum2 = 0;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i * size + i];
+		sum2 += a[i * size + (size - i - 1)];
+	}
+		printf("%d, %d\n", sum1, sum2);
+}
