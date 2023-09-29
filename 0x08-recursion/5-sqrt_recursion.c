958489fb87f9fba07a7f07cfb42b10ee1d87ef49
Mygithubayaa <ayaalmouiz11@gmail.com>
2023-09-29 08:22:46 -0700

    mycommit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,38 @@
 #include "main.h"
 
-int _sqrt_recursion_h(int n, int i);
+
+int actual_sqrt_recursion(int n, int i);
+
 
 /**
- *_sqrt_recursion - returns square root
- * @n: int
- * Return: int
+ * _sqrt_recursion - return squ root
+ * @n: num
+ * Return:int
 */
 
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	return (-1);
-	return (_sqrt_recursion_h(n, 0));
+
+	return (actual_sqrt_recursion(n, 0));
+
 }
 
+
 /**
- * _sqrt_recursion_h - help first function
- * @n: intger
+ * actual_sqrt_recursion - help first function
+ * @n: numb
  * @i: intger
  * Return: int
-*/
+ */
 
-int _sqrt_recursion_h(int n, int i)
+int actual_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
 	return (-1);
 	if (i * i == n)
 	return (i);
-	return (_sqrt_recursion_h(n, i + 1));
+	return (actual_sqrt_recursion(n, i + 1));
+}
 }
