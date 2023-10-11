d954f36da7e3db1874d6c724f02d887c3d666387
Mygithubayaa <ayaalmouiz11@gmail.com>
2023-10-11 11:38:13 -0700

    mycommit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog-dog data
+ *
+ * @name:dig name
+ *
+ * @age:dog age
+ * @owner:dog owner
+ *
+ *
+ * Description:longer description
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
